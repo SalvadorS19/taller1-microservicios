6d3874ddc24860680fc4d7a74dba4c488a0ec555
ssanabria <salvadorsanabria14@gmail.com>
2024-04-01 23:16:45 -0500

    test producto service

--- src/test/java/com/taller1/microservicios/service/ProductoServiceTest.java
+++ src/test/java/com/taller1/microservicios/service/ProductoServiceTest.java
@@ -22,6 +22,7 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class})
@@ -154,5 +155,79 @@ public class ProductoServiceTest {
     void givenId_whenDelete_DeleteProducto() {
         Long productoId = 1L;
         Producto producto = getProductoMock();
+        given(productoRepository.findById(productoId)).willReturn(Optional.of(producto));
+        doNothing().when(productoRepository).delete(producto);
+        productoService.removerProducto(productoId);
+        Assertions.assertAll(() -> productoService.removerProducto(productoId));
+    }
+
+    @Test
+    void givenTermino_whenFindByTermino_returnProductoDtoList() {
+        String palabra = "tele";
+        List<Producto> productos = new ArrayList<>();
+        productos.add(Producto.builder()
+                .id(1L)
+                .nombre("Televisor LG")
+                .stock(5)
+                .precio(500d)
+                .build()
+        );
+        productos.add(Producto.builder()
+                .id(2L)
+                .nombre("Televisor Samsung")
+                .stock(10)
+                .precio(1000d)
+                .build()
+        );
+        List<ProductoDto> productoDtoList = productos.stream().map(producto ->
+                new ProductoDto(
+                        producto.getId(),
+                        producto.getNombre(),
+                        producto.getPrecio(),
+                        producto.getStock()
+                )
+        ).toList();
+        given(productoRepository.findByNombreContainingIgnoreCase(palabra)).willReturn(productos);
+        given(productoMapper.productosListToProductosDtoList(productos)).willReturn(productoDtoList);
+        List<ProductoDto> productoDtos = productoService.buscarProductoByTermino(palabra);
+        Assertions.assertNotNull(productoDtos);
+        Assertions.assertEquals(productoDtos.size(), 2);
+    }
+
+    @Test
+    void whenFindByInStock_returnProductoDtoList() {
+        List<Producto> productos = getProductoListMock();
+        List<ProductoDto> productoDtoList = productos.stream().map(producto ->
+                new ProductoDto(
+                        producto.getId(),
+                        producto.getNombre(),
+                        producto.getPrecio(),
+                        producto.getStock()
+                )
+        ).toList();
+        given(productoRepository.findByInStock()).willReturn(productos);
+        given(productoMapper.productosListToProductosDtoList(productos)).willReturn(productoDtoList);
+        List<ProductoDto> productoDtos = productoService.buscarProductosEnStock();
+        Assertions.assertNotNull(productoDtos);
+        Assertions.assertEquals(productoDtos.size(), 2);
+    }
+    @Test
+    void givenPrecioAndStock_whenFindByLessEqualThan_returnProductoDtoList() {
+        Integer stockMax = 10;
+        Double precioMax = 2000d;
+        List<Producto> productos = getProductoListMock();
+        List<ProductoDto> productoDtoList = productos.stream().map(producto ->
+                new ProductoDto(
+                        producto.getId(),
+                        producto.getNombre(),
+                        producto.getPrecio(),
+                        producto.getStock()
+                )
+        ).toList();
+        given(productoRepository.findByPrecioLessThanEqualAndStockLessThanEqual(precioMax, stockMax)).willReturn(productos);
+        given(productoMapper.productosListToProductosDtoList(productos)).willReturn(productoDtoList);
+        List<ProductoDto> productoDtos = productoService.buscarProductoMenoresByPrecioAndStock(precioMax, stockMax);
+        Assertions.assertNotNull(productoDtos);
+        Assertions.assertEquals(productoDtos.size(), 2);
     }
 }
